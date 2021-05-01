40ed0d57260103c60c177c203c2413ce05ebfc75
Sumedha Deshpande <sumedhasd1308@gmail.com>
2021-05-02 01:22:44 +0530

    Adding index.js for TempUnitConvertor

--- TempUnitConvertor/index.js
+++ TempUnitConvertor/index.js
@@ -1,1 +1,28 @@
+const readLineSync = require('readline-sync');
 
+const celsiuseToFahrenheit = (temp) => ((9/5)*(temp) + 32);
+
+
+const fahrenheitTocelsiuse = (temp) => ((5/9)*(parseInt(temp) - 32));
+
+const urlDecoder = (url) => decodeURIComponent(url);
+
+const selectedUtilityOption = parseInt(readLineSync.question('Welcome to temperature convertor. Which utility function would you like to use?\n1. celsiuseToFahrenheit\n2. FahrenheitTocelsiuse\n'));
+
+console.log(`You selected ${selectedUtilityOption}`);
+
+switch(selectedUtilityOption) {
+  case 1:
+    const celToFar = readLineSync.question('Please enter the temperature in celsiuse\n');
+    const farTemp = celsiuseToFahrenheit(celToFar);
+    console.log('\nTemperature in Fahrenheit is : '+farTemp);
+    break;
+  case 2:
+    const farToCel = readLineSync.question('Please enter the temperature in Fahrenheit\n');
+    const celTemp = fahrenheitTocelsiuse(farToCel);
+    console.log('\nTemperature in celsiuse is : '+celTemp);
+    break;
+  default:
+    console.log('Invalid temperature\n');
+    break;
+}
